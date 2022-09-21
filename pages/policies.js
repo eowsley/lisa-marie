e37725045ed3af6a294cd2e744fd226bdbb133a3
eowsley <ekoayoga@gmail.com>
2022-09-21 16:06:12 -0700

    fixing issue

--- pages/policies.js
+++ pages/policies.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Policies } from "../src/components/Policies";
 import { Footer } from "../src/components/Shared";
+import { Contact } from "../src/components/Contact";
 import Header from "../src/components/Shared/Header";
 export default function Home() {
   return (
